a20ef1c4f0ccf315e6312b67d8d99cd8f49256f0
Stepish <vip.asvolkov@gmail.com>
2022-06-01 20:36:36 +0300

    delete

--- temp/php/db.php
+++
@@ -1,6 +0,0 @@
-<?php
-$con=new mysqli('CopyStar', 'root', '', 'copystar');
-if ($con->connect_error) {
-    echo"Ошибка подключения к БД: $con->connect_error";
-}
-mysqli_set_charset($con,'utf8md4');
